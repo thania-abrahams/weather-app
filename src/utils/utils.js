febb03f549dd436fa70fb96ef259c0395859aff7
Thania Abrahams <thaniaabrahams@Thanias-MacBook-Pro.local>
2021-07-22 00:06:52 +0200

    Formatting.

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,19 +2,24 @@ export const findClosestForecastToTime = (targetTime, forecasts) => {
 	const closestTime = forecasts
 		.map((forecast) => ({
 			dt_txt: forecast.dt_txt,
+
 			delta: Math.abs(targetTime - new Date(forecast.dt_txt).getHours()),
 		}))
 		.sort((a, b) => (a.delta > b.delta ? 1 : -1))[0];
+
 	return forecasts.find((forecast) => forecast.dt_txt === closestTime.dt_txt);
 };
 
 export const getRelativeDay = (date) => {
 	const now = new Date();
+
 	let readableDate = 'Today';
+
 	if (date.getDay() === now.getDay() + 1) {
 		readableDate = 'Tomorrow';
 	} else if (date.getDay() > now.getDay()) {
 		readableDate = date.toLocaleDateString(undefined, { weekday: 'long' });
 	}
+
 	return readableDate;
 };
